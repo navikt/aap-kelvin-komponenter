097b69fbea4a08ec7246cfb45d946e9cdefa141b
Fredrik Meyer <fredrik.meyer@nav.no>
2025-02-27 08:10:26 +0100

    Bruk test suite-plugin

--- motor-api/build.gradle.kts
+++ motor-api/build.gradle.kts
@@ -30,7 +30,6 @@ dependencies {
     testImplementation("ch.qos.logback:logback-classic:1.5.17")
     testImplementation("net.logstash.logback:logstash-logback-encoder:8.0")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 sourceSets {
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -36,6 +36,5 @@ dependencies {
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.27.3")
 }
