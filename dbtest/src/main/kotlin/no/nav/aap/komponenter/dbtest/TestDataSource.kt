2f563d4b39aad7f0199c0b255f9d82c07f3d23db
Per Henden <per.christian.henden@nav.no>
2025-10-31 13:59:33 +0100

    Oppdater kommentar

--- dbtest/src/main/kotlin/no/nav/aap/komponenter/dbtest/TestDataSource.kt
+++ dbtest/src/main/kotlin/no/nav/aap/komponenter/dbtest/TestDataSource.kt
@@ -10,13 +10,17 @@ import javax.sql.DataSource
  *
  * Bruk slik:
  * <pre><code>
- * @AutoClose
+ * companion object {
  * private val dataSource = TestDataSource()
+ *
+ * @AfterAll
+ * @JvmStatic
+ * fun tearDown() = dataSource.close()
+ * }
  * </code></pre>
  *
  * Eller dersom du trenger å nullstille databasens innhold mellom hver test:
  * <pre><code>
- * @AutoClose
  * private lateinit var dataSource: TestDataSource
  *
  * @BeforeEach
@@ -31,7 +35,7 @@ import javax.sql.DataSource
  *   ...
  * </code></pre>
  */
-public class TestDataSource private constructor(private val delegate: DataSource) : AutoCloseable, DataSource by delegate {
+class TestDataSource private constructor(private val delegate: DataSource) : AutoCloseable, DataSource by delegate {
     override fun createConnectionBuilder(): ConnectionBuilder {
         return delegate.createConnectionBuilder()
     }
@@ -40,12 +44,12 @@ public class TestDataSource private constructor(private val delegate: DataSource
         return delegate.createShardingKeyBuilder()
     }
 
-    public override fun close() {
+    override fun close() {
         InitTestDatabase.closerFor(delegate)
     }
 
-    public companion object {
-        public operator fun invoke(): TestDataSource {
+    companion object {
+        operator fun invoke(): TestDataSource {
             val delegate = InitTestDatabase.freshDatabase()
             return TestDataSource(delegate)
         }
