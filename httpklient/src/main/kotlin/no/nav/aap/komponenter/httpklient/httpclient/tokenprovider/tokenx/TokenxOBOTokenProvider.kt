0cd1964cb969b2344be9152f434707ac7f254968
Nicolas N. <nicolas.nordhagen@nav.no>
2026-04-23 12:54:05 +0200

    tokenx texas til singleton også

--- httpklient/src/main/kotlin/no/nav/aap/komponenter/httpklient/httpclient/tokenprovider/tokenx/TokenxOBOTokenProvider.kt
+++ httpklient/src/main/kotlin/no/nav/aap/komponenter/httpklient/httpclient/tokenprovider/tokenx/TokenxOBOTokenProvider.kt
@@ -1,17 +1,13 @@
 package no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.tokenx
 
-import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import no.nav.aap.komponenter.config.requiredConfigForKey
 import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.TexasOBOTokenProvider
 import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.TokenProvider
 import java.net.URI
 
-public class TokenxOBOTokenProvider(
-    texasUri: URI = URI(requiredConfigForKey("nais.token.exchange.endpoint")),
-    prometheus: MeterRegistry = SimpleMeterRegistry(),
-) : TokenProvider by TexasOBOTokenProvider(
-    texasUri = texasUri,
+public object TokenxOBOTokenProvider : TokenProvider by TexasOBOTokenProvider(
+    texasUri = URI(requiredConfigForKey("nais.token.exchange.endpoint")),
     identityProvider = "tokenx",
-    prometheus = prometheus,
+    prometheus = SimpleMeterRegistry(),
 )
