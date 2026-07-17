3e53c91389d2d2811167fdfe9e4e4f13dbe2daac
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2026-07-17 09:51:19 +0200

    AAP-2194 Dockstring, serde og hjelpemetode for Bruker

--- verdityper/src/main/kotlin/no/nav/aap/komponenter/verdityper/Bruker.kt
+++ verdityper/src/main/kotlin/no/nav/aap/komponenter/verdityper/Bruker.kt
@@ -1,3 +1,26 @@
 package no.nav.aap.komponenter.verdityper
 
-public data class Bruker(public val ident: String)
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+
+/** Representerer en Nav-ansatt (eller en annen person som er logget
+ * inn i Navs systemer på samme måte som ansatte).
+ *
+ * Se [PersonBruker] for tilsvarende klasse som representerer en privat
+ * person (med fødselsnummer, d-nummer, o.l.).
+ */
+public data class Bruker
+@JsonCreator constructor(
+    /** Ident til innlogget bruker. Typisk på formet X123456. */
+    @JsonValue
+    public val ident: String,
+) {
+
+    /** Er det en ekte person, med Nav-ident?
+     *
+     * Klassen brukes også for systembrukere (`Kelvin` f.eks.).
+     */
+    public fun erNavIdent(): Boolean {
+        return this.ident.matches(Regex("\\w\\d{6}"))
+    }
+}
