adf18867317929e394a21805376e4a0934688542
Fredrik Meyer <fredrik.meyer@nav.no>
2026-08-18 14:07:13 +0200

    AAP-BAU Følg Gradle best practices (#517)

--- build-logic/src/main/kotlin/aap.conventions.gradle.kts
+++ build-logic/src/main/kotlin/aap.conventions.gradle.kts
@@ -11,26 +11,20 @@ plugins {
 group = "no.nav.aap.kelvin"
 version = project.findProperty("version")?.toString() ?: "0.0.0"
 
-repositories {
-    mavenCentral()
-    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
-}
-
-
 val jacksonVersion = the<VersionCatalogsExtension>().named("libs").findVersion("jackson3").get().requiredVersion
 
 dependencies {
-    constraints {
-        // Sikkerhetspin – sårbarhet i tools.jackson.core 3.1.1
-        add("implementation", "tools.jackson.core:jackson-databind") {
-            version { strictly(jacksonVersion) }
-            because("Sikkerhetsfiks – sårbarhet i 3.1.1")
-        }
-        add("implementation", "tools.jackson.core:jackson-core") {
-            version { strictly(jacksonVersion) }
-            because("Sikkerhetsfiks – sårbarhet i 3.1.1")
-        }
-    }
+//    constraints {
+//        // Sikkerhetspin – sårbarhet i tools.jackson.core 3.1.1
+//        add("implementation", "tools.jackson.core:jackson-databind") {
+//            version { strictly(jacksonVersion) }
+//            because("Sikkerhetsfiks – sårbarhet i 3.1.1")
+//        }
+//        add("implementation", "tools.jackson.core:jackson-core") {
+//            version { strictly(jacksonVersion) }
+//            because("Sikkerhetsfiks – sårbarhet i 3.1.1")
+//        }
+//    }
 }
 
 // https://docs.gradle.org/8.12.1/userguide/jvm_test_suite_plugin.html
